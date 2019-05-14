3e40e54d32a083739a17f503cdc88a389f470c7f
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2019-05-14 20:40:46 +0900

    initial commit

---
+++ Model.py
@@ -0,0 +1,23 @@
+
+
+class Model:
+    self.nodes = []
+
+    def __init__(self, iteration_):
+        self.iteration = iteration_
+
+    def setNode(self, n_):
+        self.nodes = n_
+
+    def setlayout(self):
+        for i in range(self.iteration):
+            for n1 in self.nodes:
+                for n2 in self.nodes:
+                    if n1 is not n2:
+                        self.calculate(n1, n2)
+
+    def isNext(self, n1_, n2_):
+        return True
+
+    def calculate(self, n1_, n2_):
+        if isNext(n1_, n2_):
---
+++ Node.py
@@ -0,0 +1,5 @@
+
+class Node:
+
+    def __init__(self, x_, y_):
+        self.pos = (x_, y_)
